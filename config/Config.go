05f341bd32ade28354806738677ddb55383a121c
zhike.feng <zhike.feng@dji.com>
2022-03-11 10:59:37 +0800

    修改配置为根据当前环境选择

--- config/Config.go
+++ config/Config.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 	"gopkg.in/ini.v1"
 	"log"
+	"runtime"
 )
 
 type ServerConfig struct {
 	Port int `ini:"port"`
 }
 type FileConfig struct {
-	FileDirPath       string `ini:"fileDirPath"`       // 文件存放根目录
-	FileAccessDirPath string `ini:"fileAccessDirPath"` // nginx配置的此根目录的访问路径
+	FileDirPath        string // 根据当前系统选择的存放文件的目录
+	FileDirPathOfWin   string `ini:"fileDirPathOfWin"`   // windows文件存放根目录
+	FileDirPathOfLinux string `ini:"fileDirPathOfLinux"` // Linux文件存放根目录
+	FileAccessDirPath  string `ini:"fileAccessDirPath"`  // nginx配置的此根目录的访问路径
 }
 type ImageConfig struct {
-	Suffix             []string `ini:"suffix"`             // 图片格式
-	FixWidth           int      `ini:"fixWidth"`           // 压缩至此宽度px
-	ToCompressSize     int      `ini:"toCompressSize"`     // 图片达此限制将被压缩，单位byte
-	ImageDirPath       string   `ini:"imageDirPath"`       // 放图片的目录
-	ImageAccessDirPath string   `ini:"imageAccessDirPath"` // nginx配置的访问存放图片目录的访问路劲
+	Suffix              []string `ini:"suffix"`         // 图片格式
+	FixWidth            int      `ini:"fixWidth"`       // 压缩至此宽度px
+	ToCompressSize      int      `ini:"toCompressSize"` // 图片达此限制将被压缩，单位byte
+	ImageDirPath        string   // 根据当前系统选择的存放图片的目录
+	ImageDirPathOfWin   string   `ini:"imageDirPathOfWin"`   // windows放图片的目录
+	ImageDirPathOfLinux string   `ini:"imageDirPathOfLinux"` // linux放图片的目录
+	ImageAccessDirPath  string   `ini:"imageAccessDirPath"`  // nginx配置的访问存放图片目录的访问路劲
 }
 
 var (
@@ -44,6 +49,16 @@ func init() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// 根据当前系统环境，选择文件和图片的存放目录
+	if runtime.GOOS == "windows" {
+		fileConfig.FileDirPath = fileConfig.FileDirPathOfWin
+		imageConfig.ImageDirPath = imageConfig.ImageDirPathOfWin
+	} else {
+		fileConfig.FileDirPath = fileConfig.FileDirPathOfLinux
+		imageConfig.ImageDirPath = imageConfig.ImageDirPathOfLinux
+	}
+	fmt.Printf("当前系统环境为%s, FileDirPATH选择为：%s， ImageDirPATH选择为：%s\n", runtime.GOOS, fileConfig.FileDirPath, imageConfig.ImageDirPath)
 	fmt.Printf("读取my.ini配置文件，解析如下:\n"+
 		"ServerConfig: %+v\n"+
 		"FileConfig: %+v\n"+
